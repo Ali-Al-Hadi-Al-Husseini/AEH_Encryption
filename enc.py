7a52d25ef5229a89eca9f8e8790b193c22bca8d0
Ali-Al-Hadi-Al-Husseini <77203019+Ali-Al-Hadi-Al-Husseini@users.noreply.github.com>
2021-01-21 09:46:26 +0000

    made everything in a class

--- enc.py
+++ enc.py
@@ -1,23 +1,25 @@
 from hashlib import sha256
 
-def create_list(key,size):
-    x = int(size/64) + 1
-    this_list= [sha256(key.encode()).hexdigest()]
-    this_list.extend(split_hash(this_list[0]))
 
-    for i in range(x-1):
-        x = ((len(this_list)+1)//2 ) -1
-        print(x)
-        for i in this_list[x:]:
-            this_list.extend(split_hash(i)) 
-            
+    
+class enc:
+    @classmethod
+    def create_list(self,key,size):
+        x = int(size/64) + 1
+        this_list= [sha256(key.encode()).hexdigest()]
+        this_list.extend(self.split_hash(this_list[0]))
 
-    return this_list
+        for i in range(x-1):
+            y = ((len(this_list)+1)//2 ) -1
+            for i in this_list[y:]:
+                this_list.extend(self.split_hash(i)) 
+                
 
-def split_hash(key):
-    x = int(len(key) /2)
-    return list([sha256(key[x:].encode()).hexdigest(),sha256(key[:x].encode()).hexdigest()])
-
+        return this_list
+    @classmethod
+    def split_hash(self,key):
+        x = int(len(key) /2)
+        return list([sha256(key[x:].encode()).hexdigest(),sha256(key[:x].encode()).hexdigest()])
 
     
 
