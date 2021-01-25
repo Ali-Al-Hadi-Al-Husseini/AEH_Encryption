599b6951f3b2026e569755299171796fffb9c1d3
Ali-Al-Hadi-Al-Husseini <77203019+Ali-Al-Hadi-Al-Husseini@users.noreply.github.com>
2021-01-25 20:46:25 +0000

    Update enc.py

--- enc.py
+++ enc.py
@@ -8,18 +8,22 @@ class enc:
         x = int(size/64) + 1
         this_list= [sha256(key.encode()).hexdigest()]
         this_list.extend(self.split_hash(this_list[0]))
+        srt_k = ''
 
         for i in range(x-1):
             y = ((len(this_list)+1)//2 ) -1
             for i in this_list[y:]:
                 this_list.extend(self.split_hash(i)) 
-                
+        for i in this_list:
+            srt_k += i
 
-        return this_list
+        return str_k
     @classmethod
     def split_hash(self,key):
         x = int(len(key) /2)
         return list([sha256(key[x:].encode()).hexdigest(),sha256(key[:x].encode()).hexdigest()])
+    
+    
 
     
 
