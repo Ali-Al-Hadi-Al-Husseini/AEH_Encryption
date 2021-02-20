11f93feb4eee07c3b58d15483e6afa63ada17839
Ali-Al-Hadi-Al-Husseini <77203019+Ali-Al-Hadi-Al-Husseini@users.noreply.github.com>
2021-02-20 17:32:13 +0000

    Update enc.py
    made the 5 num genetator

--- enc.py
+++ enc.py
@@ -1,20 +1,20 @@
 from hashlib import sha256
-
+from os import remove
 
     
 class Enc:
 
     # creater a string that is as the size of the file
     @classmethod
-    def create_list(cls,Key,size):
+    def create_list1(cls,Key,size):
         if size <= 64:
             new_key = []
-            new_key.append(Key)
+            new_key.append(sha256(Key.encode()).hexdigest())
             return new_key
 
         else:
             new_keys = []
-            new_list = cls.create_new_list( cls.create_list( Key,(size * 5) // 2))
+            new_list = cls.create_new_list( cls.create_list1( Key,(size // 2)))
             new_keys.extend( new_list)
             return new_keys
 
@@ -39,6 +39,66 @@ class Enc:
         with open('keys.txt', 'w') as temp:
             for key in keys_list:
                 temp.write(key)
+    @classmethod
+    def create_list(cls,key,size):
+        return cls.create_list1(key,size*5)
 
 
+    @classmethod
+    def generate_keylist(cls,txt_size,key):
+        cls.create_file(key, txt_size)
+        num_list = []
+        with open('keys.txt','r') as keys:
+            key = list(keys.read())
+            
+            for idx in range(1, int(len(key) // 5) + 1):
+                if idx > len(key):
+                    break
+
+                else:
+                    temp_list = key[(idx * 5) - 5:idx * 5]
+                    num = cls.nums(temp_list)
+                    num_list.append((num % 83) +39)
+            
+        remove('keys.txt')
+        return num_list 
+    @classmethod
+    def compare(cls,num):
+        if num % 2 == 0:
+            num = num*7
+        elif  (num % 81 ) <= 34:
+            num = 36 * num
+        else:
+            num = ((12 * num) * 7)
+        return num
+
+    @classmethod
+    def nums(cls,temp_list):
+
+        for idx in range(len(temp_list)):
+            if type(temp_list[idx]) == str:
+                temp_list[idx] = ord(temp_list[idx])
+
+        num2 = int(((( temp_list[1] ** temp_list[3]) + temp_list[2]) * temp_list[4] ) // temp_list[0])
+        num3 = int((7 ** (temp_list[0] + temp_list[1] + temp_list[2] + temp_list[3] + temp_list[4])) // 3)
+        num1 = 0
+        try:
+            num1 += int(((( temp_list[2] ** temp_list[1]) + temp_list[0]) * temp_list[4] ) // temp_list[3])
+                        
+        except ZeroDivisionError:
+            try :
+                num1 = num2
+
+            except ZeroDivisionError:
+                num1 = num3    
+        if num1 != 0:
+            return num1
+
+        else:
+            print('this key is not safe enough') 
+            return None
+        
+    @classmethod
+    def get_dicts(cls):
+        return {0: "'", 1: '(', 2: ')', 3: '*', 4: '+', 5: ',', 6: '-', 7: '.', 8: '/', 9: '0', 10: '1', 11: '2', 12: '3', 13: '4', 14: '5', 15: '6', 16: '7', 17: '8', 18: '9', 19: ':', 20: ';', 21: '<', 22: '=', 23: '>', 24: '?', 25: '@', 26: 'A', 27: 'B', 28: 'C', 29: 'D', 30: 'E', 31: 'F', 32: 'G', 33: 'H', 34: 'I', 35: 'J', 36: 'K', 37: 'L', 38: 'M', 39: 'N', 40: 'O', 41: 'P', 42: 'Q', 43: 'R', 44: 'S', 45: 'T', 46: 'U', 47: 'V', 48: 'W', 49: 'X', 50: 'Y', 51: 'Z', 52: '[', 53: '\\', 54: ']', 55: '^', 56: '_', 57: '`', 58: 'a', 59: 'b', 60: 'c', 61: 'd', 62: 'e', 63: 'f', 64: 'g', 65: 'h', 66: 'i', 67: 'j', 68: 'k', 69: 'l', 70: 'm', 71: 'n', 72: 'o', 73: 'p', 74: 'q', 75: 'r', 76: 's', 77: 't', 78: 'u', 79: 'v', 80: 'w', 81: 'x', 82: 'y', 83: 'z', 84: ' '} , {"'": 0, '(': 1, ')': 2, '*': 3, '+': 4, ',': 5, '-': 6, '.': 7, '/': 8, '0': 9, '1': 10, '2': 11, '3': 12, '4': 13, '5': 14, '6': 15, '7': 16, '8': 17, '9': 18, ':': 19, ';': 20, '<': 21, '=': 22, '>': 23, '?': 24, '@': 25, 'A': 26, 'B': 27, 'C': 28, 'D': 29, 'E': 30, 'F': 31, 'G': 32, 'H': 33, 'I': 34, 'J': 35, 'K': 36, 'L': 37, 'M': 38, 'N': 39, 'O': 40, 'P': 41, 'Q': 42, 'R': 43, 'S': 44, 'T': 45, 'U': 46, 'V': 47, 'W': 48, 'X': 49, 'Y': 50, 'Z': 51, '[': 52, '\\': 53, ']': 54, '^': 55, '_': 56, '`': 57, 'a': 58, 'b': 59, 'c': 60, 'd': 61, 'e': 62, 'f': 63, 'g': 64, 'h': 65, 'i': 66, 'j': 67, 'k': 68, 'l': 69, 'm': 70, 'n': 71, 'o': 72, 'p': 73, 'q': 74, 'r': 75, 's': 76, 't': 77, 'u': 78, 'v': 79, 'w': 80, 'x': 81, 'y': 82, 'z': 83, ' ': 84}
                 
