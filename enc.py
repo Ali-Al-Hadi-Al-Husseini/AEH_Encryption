1ffb40cb8b6726239e2335967ebbf1a37a8c5f83
Ali-Al-Hadi-Al-Husseini <77203019+Ali-Al-Hadi-Al-Husseini@users.noreply.github.com>
2021-03-01 15:48:57 +0000

    add 2d array manipulation

--- enc.py
+++ enc.py
@@ -103,7 +103,64 @@ class Enc:
         dict1 = {0: "'", 1: '(', 2: ')', 3: '*', 4: '+', 5: ',', 6: '-', 7: '.', 8: '/', 9: '0', 10: '1', 11: '2', 12: '3', 13: '4', 14: '5', 15: '6', 16: '7', 17: '8', 18: '9', 19: ':', 20: ';', 21: '<', 22: '=', 23: '>', 24: '?', 25: '@', 26: 'A', 27: 'B', 28: 'C', 29: 'D', 30: 'E', 31: 'F', 32: 'G', 33: 'H', 34: 'I', 35: 'J', 36: 'K', 37: 'L', 38: 'M', 39: 'N', 40: 'O', 41: 'P', 42: 'Q', 43: 'R', 44: 'S', 45: 'T', 46: 'U', 47: 'V', 48: 'W', 49: 'X', 50: 'Y', 51: 'Z', 52: '[', 53: '\\', 54: ']', 55: '^', 56: '_', 57: '`', 58: 'a', 59: 'b', 60: 'c', 61: 'd', 62: 'e', 63: 'f', 64: 'g', 65: 'h', 66: 'i', 67: 'j', 68: 'k', 69: 'l', 70: 'm', 71: 'n', 72: 'o', 73: 'p', 74: 'q', 75: 'r', 76: 's', 77: 't', 78: 'u', 79: 'v', 80: 'w', 81: 'x', 82: 'y', 83: 'z', 84: ' '} 
         dict2 = {"'": 0, '(': 1, ')': 2, '*': 3, '+': 4, ',': 5, '-': 6, '.': 7, '/': 8, '0': 9, '1': 10, '2': 11, '3': 12, '4': 13, '5': 14, '6': 15, '7': 16, '8': 17, '9': 18, ':': 19, ';': 20, '<': 21, '=': 22, '>': 23, '?': 24, '@': 25, 'A': 26, 'B': 27, 'C': 28, 'D': 29, 'E': 30, 'F': 31, 'G': 32, 'H': 33, 'I': 34, 'J': 35, 'K': 36, 'L': 37, 'M': 38, 'N': 39, 'O': 40, 'P': 41, 'Q': 42, 'R': 43, 'S': 44, 'T': 45, 'U': 46, 'V': 47, 'W': 48, 'X': 49, 'Y': 50, 'Z': 51, '[': 52, '\\': 53, ']': 54, '^': 55, '_': 56, '`': 57, 'a': 58, 'b': 59, 'c': 60, 'd': 61, 'e': 62, 'f': 63, 'g': 64, 'h': 65, 'i': 66, 'j': 67, 'k': 68, 'l': 69, 'm': 70, 'n': 71, 'o': 72, 'p': 73, 'q': 74, 'r': 75, 's': 76, 't': 77, 'u': 78, 'v': 79, 'w': 80, 'x': 81, 'y': 82, 'z': 83, ' ': 84}
         return dict1, dict2
+    #this part is not doe yet
     @classmethod
     def generate_dicts(cls,key):
         #this is not doneyet but this should generate new distribution key value pair in the dictionaries 
         pass
+    @classmethod
+    def convert_to_dict(cls,character_list):
+        dict1 = {}
+
+        for i in len(character_list):
+            dict1[i] = character_list[i]
+
+        return dict1
+       
+    @classmethod
+    def reverse_dict(cls,dict1):
+        new_dict = {}
+
+        for key, value in dict1:
+            new_dict[value] = key
+
+        return new_dict
+
+    @classmethod
+    def generate_dict(key):
+        character_list = ["'", '(', ')', '*', '+', ',', '-', '.', '/', '0',
+                          '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=',
+                          '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+                          'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+                          'X', 'Y', 'Z', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd',
+                          'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+                          'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+        return character_list
+    #till hear is now finished yet
+    @classmethod
+    def matrix_manipultion(cls,txt, key):
+    new_nums = []
+    nums = []
+    temp_num = [1, 3, 0, 2]
+    for idx in range(1, len(key) // 8):
+        nums = key[(idx - 1) * 8: idx * 8 - 1]
+        try:
+            x = cls.num(nums) % (len(temp_num))
+            new_nums.append(temp_num.pop(x))
+        except ZeroDivisionError:
+            pass
+    return new_nums
+
+    @classmethod
+    def num(cls,nums):
+        new_num = 0
+        for idx in range(len(nums)):
+            new_num += ord(nums[idx])
+
+        return new_num
+    # this part need to be wired
+
+
+    
+    
+    
