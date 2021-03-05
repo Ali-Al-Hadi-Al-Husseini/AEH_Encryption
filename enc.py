70028df5c4fd62431a6d2bf25146c49f5ab4b335
Ali-Al-Hadi-Al-Husseini <77203019+Ali-Al-Hadi-Al-Husseini@users.noreply.github.com>
2021-03-05 19:13:54 +0000

    added dict generation

--- enc.py
+++ enc.py
@@ -4,9 +4,9 @@ from os import remove
     
 class Enc:
 
-    # creater a string that is as the size of the file
+    # creater a string that is as the size of the file from hashes using sha256
     @classmethod
-    def create_list1(cls,Key,size):
+    def create_list(cls,Key,size):
         if size <= 64:
             new_key = []
             new_key.append(sha256(Key.encode()).hexdigest())
@@ -18,12 +18,13 @@ class Enc:
             new_keys.extend( new_list)
             return new_keys
 
-    #this method take one parameter (hash as a string) and then returns the hash of the half of the key
+    #this method take one parameter (hash as a string) and then returns the hashs of the two halfs of the key
     @classmethod
     def split_and_hash(cls,key):
         half_key = int(len(key) /2) + 1
         return list((sha256(key[half_key:].encode()).hexdigest(),sha256(key[:half_key].encode()).hexdigest()))
-
+    
+    #takes a list of hashes and return a twice as big list from spliting anf hashing each hash
     @classmethod
     def create_new_list(cls,keys):
         new_keys = []
@@ -32,18 +33,16 @@ class Enc:
 
         return new_keys
 
-    #write the last keys the keys.txt file
+    #write the  generated list from create_new_list in a file called keys.txt
     @classmethod
     def  create_file(cls,key,size):
-        keys_list = cls.create_list(key,size)
+        keys_list = cls.create_list(key,size * 5)
         with open('keys.txt', 'w') as temp:
             for key in keys_list:
                 temp.write(key)
-    @classmethod
-    def create_list(cls,key,size):
-        return cls.create_list1(key,size*5)
-
-
+                
+    """ takes the size of the text that to be encrypted  and takes the key to genrate the file (keys.txt) using the class
+     method create_file adn then take  the  series of hashes and pass it to the nums classmethod"""
     @classmethod
     def generate_keylist(cls,txt_size,key):
         cls.create_file(key, txt_size)
@@ -58,56 +57,47 @@ class Enc:
                 else:
                     temp_list = key[(idx * 5) - 5:idx * 5]
                     num = cls.nums(temp_list)
-                    num_list.append((num % 83) +39)
+                    num_list.append((num % 83) + 39)
             
         remove('keys.txt')
-        return num_list 
-    @classmethod
-    def compare(cls,num):
-        if num % 2 == 0:
-            num = num*7
-        elif  (num % 81 ) <= 34:
-            num = 36 * num
-        else:
-            num = ((12 * num) * 7)
-        return num
-
+        return num_list
+    
+    """ takes  a list of len 5 and genrate a number from it to pass it back to  generate_keylist """
     @classmethod
     def nums(cls,temp_list):
-
+        #checks to see if ther are any letter in the list and then change it to numbers 
         for idx in range(len(temp_list)):
             if type(temp_list[idx]) == str:
                 temp_list[idx] = ord(temp_list[idx])
-
+                
+        #this block genrates new number from the five number in the temp_list
         num2 = int(((( temp_list[1] ** temp_list[3]) + temp_list[2]) * temp_list[4] ) // temp_list[0])
         num3 = int((7 ** (temp_list[0] + temp_list[1] + temp_list[2] + temp_list[3] + temp_list[4])) // 3)
         num1 = 0
+        
         try:
             num1 += int(((( temp_list[2] ** temp_list[1]) + temp_list[0]) * temp_list[4] ) // temp_list[3])
-                        
+            #in cases where temp_list[3] can be zero so thats why we used try/execpt here 
+            
         except ZeroDivisionError:
+            #provides two alternative number if num1 has an error
             try :
                 num1 = num2
 
             except ZeroDivisionError:
+                """num3 cann't have an error because it uses only addtion it can only be 0 if all the i
+                tems in temp_list is zero which is to rare to happen """
+                
                 num1 = num3    
         if num1 != 0:
+            
             return num1
 
         else:
             print('this key is not safe enough') 
             return None
-        
-   @classmethod
-    def get_dicts(cls):
-        dict1 = {0: "'", 1: '(', 2: ')', 3: '*', 4: '+', 5: ',', 6: '-', 7: '.', 8: '/', 9: '0', 10: '1', 11: '2', 12: '3', 13: '4', 14: '5', 15: '6', 16: '7', 17: '8', 18: '9', 19: ':', 20: ';', 21: '<', 22: '=', 23: '>', 24: '?', 25: '@', 26: 'A', 27: 'B', 28: 'C', 29: 'D', 30: 'E', 31: 'F', 32: 'G', 33: 'H', 34: 'I', 35: 'J', 36: 'K', 37: 'L', 38: 'M', 39: 'N', 40: 'O', 41: 'P', 42: 'Q', 43: 'R', 44: 'S', 45: 'T', 46: 'U', 47: 'V', 48: 'W', 49: 'X', 50: 'Y', 51: 'Z', 52: '[', 53: '\\', 54: ']', 55: '^', 56: '_', 57: '`', 58: 'a', 59: 'b', 60: 'c', 61: 'd', 62: 'e', 63: 'f', 64: 'g', 65: 'h', 66: 'i', 67: 'j', 68: 'k', 69: 'l', 70: 'm', 71: 'n', 72: 'o', 73: 'p', 74: 'q', 75: 'r', 76: 's', 77: 't', 78: 'u', 79: 'v', 80: 'w', 81: 'x', 82: 'y', 83: 'z', 84: ' '} 
-        dict2 = {"'": 0, '(': 1, ')': 2, '*': 3, '+': 4, ',': 5, '-': 6, '.': 7, '/': 8, '0': 9, '1': 10, '2': 11, '3': 12, '4': 13, '5': 14, '6': 15, '7': 16, '8': 17, '9': 18, ':': 19, ';': 20, '<': 21, '=': 22, '>': 23, '?': 24, '@': 25, 'A': 26, 'B': 27, 'C': 28, 'D': 29, 'E': 30, 'F': 31, 'G': 32, 'H': 33, 'I': 34, 'J': 35, 'K': 36, 'L': 37, 'M': 38, 'N': 39, 'O': 40, 'P': 41, 'Q': 42, 'R': 43, 'S': 44, 'T': 45, 'U': 46, 'V': 47, 'W': 48, 'X': 49, 'Y': 50, 'Z': 51, '[': 52, '\\': 53, ']': 54, '^': 55, '_': 56, '`': 57, 'a': 58, 'b': 59, 'c': 60, 'd': 61, 'e': 62, 'f': 63, 'g': 64, 'h': 65, 'i': 66, 'j': 67, 'k': 68, 'l': 69, 'm': 70, 'n': 71, 'o': 72, 'p': 73, 'q': 74, 'r': 75, 's': 76, 't': 77, 'u': 78, 'v': 79, 'w': 80, 'x': 81, 'y': 82, 'z': 83, ' ': 84}
-        return dict1, dict2
-    #this part is not doe yet
-    @classmethod
-    def generate_dicts(cls,key):
-        #this is not doneyet but this should generate new distribution key value pair in the dictionaries 
-        pass
+       
+    # converts  a list which is normally passed from the class method generate_new_dicts
     @classmethod
     def convert_to_dict(cls,character_list):
         dict1 = {}
@@ -116,7 +106,8 @@ class Enc:
             dict1[i] = character_list[i]
 
         return dict1
-       
+    
+    # returns a new dict where the keys are the value and vice versa
     @classmethod
     def reverse_dict(cls,dict1):
         new_dict = {}
@@ -125,9 +116,11 @@ class Enc:
             new_dict[value] = key
 
         return new_dict
-
+    
+    """take a key and generates a new list with different order  of the letters (changes the index of each letter and give a new_list)
+    which means every key have different order of letter which makes it a bit harder to crack it """
     @classmethod
-    def generate_dict(key):
+    def generate_new_dict(cls, key):
         character_list = ["'", '(', ')', '*', '+', ',', '-', '.', '/', '0',
                           '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=',
                           '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
@@ -135,8 +128,15 @@ class Enc:
                           'X', 'Y', 'Z', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd',
                           'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
                           'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
-        return character_list
-    #till hear is now finished yet
+
+        new_character_list = []
+        keys = cls.generate_keylist(len(character_list), key)
+        for idx in range(len(character_list)):
+            new_character_list.append(character_list.pop(
+                keys[idx] % len(character_list)))
+
+        return new_character_list
+    # changes the order of each letter in the txt  passed which give an exrtra layer of protection
     @classmethod
     def matrix_manipultion(cls,txt, key):
     new_nums = []
