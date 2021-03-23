e73702b87db3f194ca329b5198a6f1810481b12c
Ali-Al-Hadi-Al-Husseini <77203019+Ali-Al-Hadi-Al-Husseini@users.noreply.github.com>
2021-03-23 17:47:29 +0000

    Update and rename encrypt.py to AEH.py

--- AEH.py
+++ AEH.py
@@ -1,41 +1,38 @@
 
-from hashlib import sha256
 from enc import Enc
+from os import remove
+from hashlib import sha256
 
 class AEH:
     # encrypts the text that was given
 
     @classmethod
     def Encrypt(cls,text, key):
+        hashed_key = sha256(key.encode()).hexdigest()
         len_text = len(text)
-        num_list = Enc.generate_keylist(len_text, key)
+        num_list = Enc.generate_keylist(len_text * 1.5, hashed_key)
         encoded = ''
-        dict1, dict2 = Enc.get_dicts()
-
+        dict1, dict2 = Enc.get_dicts(key)
         for idx in range(len_text):
             temp_num = dict2[text[idx]] + num_list[idx]
-            temp_char = dict1[( temp_num % 85)]
+            temp_char = dict1[( temp_num % len(dict1))]
             encoded += temp_char
 
+        remove('keys.txt')
         return encoded
 
     @classmethod
     def Decrypt(cls,endcoded,key):
         len_text = len(endcoded)
-        num_list = Enc.generate_keylist(len_text, key)
+        hashed_key = sha256(key.encode()).hexdigest()
+        num_list = Enc.generate_keylist(len_text, hashed_key)
         decoded = ''
-        dict1, dict2 = Enc.get_dicts()
+        dict1, dict2 = Enc.get_dicts(key)
 
         for idx in range(len_text):
-            temp_num = (dict2[endcoded[idx]]-num_list[idx]) % 85
+            temp_num = (dict2[endcoded[idx]]-num_list[idx]) % len(dict1)
             temp_char = dict1[temp_num]
             decoded += temp_char
-
+        remove('keys.txt')
         return decoded
     
-
-x = 'mousdylilo98'
-y = 'lit'
-z = AEH.Encrypt(x,y)
-print(z)
-print(AEH.Decrypt(z,y))
