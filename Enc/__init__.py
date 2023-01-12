a1839bd386cbc153994fad10dec52812d982776f
Ali-Al-Hadi-Al-Husseini <alihadi.alhusseini@gmail.com>
2023-01-13 00:42:04 +0200

    added realtive imports

--- Enc/__init__.py
+++ Enc/__init__.py
@@ -5,11 +5,12 @@ sys.path.append('c:\\Users\\lilo\\Documents\\GitHub\\AEH_Encryption\\Enc')
 sys.path.append('c:\\Users\\lilo\\Documents\\GitHub\\AEH_Encryption\\Enc\\block')
 sys.path.append('c:\\Users\\lilo\\Documents\\GitHub\\AEH_Encryption\\Enc\\Encryption_Tools')
 
-from block.Block_tools import Block_Tools,Shuffle,Block
-from Encryption_Tools.characters_list import get_characters_list
-from Encryption_Tools.Dict_tools import Dict_Tools
-from Encryption_Tools.keys_tools import Keys
-from Encryption_Tools.String_tools import String_tools 
-from AEH import AE
+
+from .block.Block_tools import Block_Tools,Shuffle,Block
+from .Encryption_Tools.characters_list import get_characters_list
+from .Encryption_Tools.Dict_tools import Dict_Tools
+from .Encryption_Tools.keys_tools import Keys
+from .Encryption_Tools.String_tools import String_tools 
+from .AEH import AE
 
 __all__ = ['Block_Tools','Shuffle','Block','get_characters_list','Dict_Tools','Keys','String_tools','AE']
--- tests/enc.py
+++ tests/enc.py
@@ -6,7 +6,7 @@ sys.path.append('c:\\Users\\lilo\\Documents\\GitHub\\AEH_Encryption\\Enc\\block'
 sys.path.append('c:\\Users\\lilo\\Documents\\GitHub\\AEH_Encryption\\Enc\\Encryption_Tools')
 
 
-from Enc import *
+from ..Enc import *
 
 
 __all__ = ['Block_Tools','Shuffle','Block','get_characters_list','Dict_Tools','Keys','String_tools','AE']
