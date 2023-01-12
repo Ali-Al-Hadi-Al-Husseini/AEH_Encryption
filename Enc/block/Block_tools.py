6dabb00a8698b3bd0329b6d19308a8895d9e1bcb
Ali-Al-Hadi-Al-Husseini <alihadi.alhusseini@gmail.com>
2023-01-12 21:34:58 +0200

    added shuffle class

--- Enc/block/Block_tools.py
+++ Enc/block/Block_tools.py
@@ -131,3 +131,93 @@ class Block_Tools:
         for process in processs:
             process.join()
 
+class Shuffle:
+    @classmethod
+    def generate_shuffle_list(cls,len_txt,org_len,key):
+        
+        new_nums = []
+        nums = []
+        temp_num = list(range(org_len ))
+        nums = Keys.generate_keylist(len_txt, key)
+
+        for idx in range(org_len):
+            new_idx = nums[idx] % (len(temp_num))
+            new_nums.append(temp_num[new_idx])
+            
+            temp_num[-1], temp_num[new_idx] =  temp_num[new_idx], temp_num[-1]
+            temp_num.pop()
+
+        return new_nums
+
+
+    @classmethod
+    def shuffle(cls, txt, key):
+        new_txt = empty(len(txt),dtype='<U2')
+        row_shifts = cls.generate_shuffle_list(len(txt) * 2 , len(txt), key)
+
+        for idx in range(len(txt)):
+            new_txt[idx] = txt[row_shifts[idx]]
+
+        return ''.join(new_txt)
+
+
+    @classmethod
+    def un_shuffle(cls, txt, key):
+        shifts = cls.generate_shuffle_list(len(txt) * 2, len(txt) , key)
+        new_list = empty(len(txt),dtype='<U2')
+
+        for idx in range(len(shifts)):
+            new_list[shifts[idx]] = txt[idx]
+            
+        return cls.convert_to_str(new_list)
+        
+
+    @classmethod
+    def convert_to_str(cls,chars_list):
+        return ''.join(chars_list)
+
+    @classmethod
+    def shuffle_bin(cls,txt,key):
+        temp = txt 
+        keys = Keys.create_list(key,(64)*2)
+
+        for idx in range(len(keys)):
+            temp = cls.shuffle_helper(temp,keys[idx])
+
+        return temp
+
+    @classmethod
+    def un_shuffle_bin(cls,txt,key):
+        temp = txt 
+        keys = Keys.create_list(key,(64)*2)
+
+        for idx in range(len(keys)):
+            temp = cls.un_shuffle_helper(temp,keys[len(keys)-1-idx])
+        return temp
+
+    @classmethod
+    def shuffle_helper(cls,txt,key):
+        dict1,dict2 = Dict_Tools.get_dicts(key)
+        new_txt = []
+
+        for char in txt:
+            temp = bin(dict2[char])[2:]  
+            temp = ''.join(['0' for _ in range(len(temp),8)]) + temp
+            new_txt.append(temp)    
+
+        shuffled_bytes =  Block_Tools.split_to_parts(cls.shuffle(''.join(new_txt),key),8)
+
+        return ''.join(dict1[int(block.bytes,2)] for block in shuffled_bytes)
+
+    @classmethod
+    def un_shuffle_helper(cls,txt,key):
+        dict1,dict2 = Dict_Tools.get_dicts(key)
+        new_txt = []
+
+        for char in txt:
+            temp = bin(dict2[char])[2:]  
+            temp = ''.join(['0' for _ in range(len(temp),8)]) + temp
+            new_txt.append(temp)  
+
+        shuffled_bytes =  Block_Tools.split_to_parts(cls.un_shuffle(''.join(new_txt),key),8)
+        return ''.join(dict1[int(block.bytes,2)] for block in shuffled_bytes)
