084b83e3b36964629ce0e8257c622168afa67056
Ali-Al-Hadi-Al-Husseini <alihadi.alhusseini@gmail.com>
2023-01-30 06:37:07 +0200

    integrated custom hashin in split and hash

--- Enc/Encryption_Tools/keys_tools.py
+++ Enc/Encryption_Tools/keys_tools.py
@@ -8,6 +8,7 @@ class Keys:
     @classmethod
     def str_to_sha256(cls,txt:str) -> str:
         return sha256(txt.encode()).hexdigest()
+
     @classmethod
     def str_to_sha3_256(cls,txt:str) -> str:
         return sha3_256(txt.encode()).hexdigest()
@@ -50,13 +51,18 @@ class Keys:
                 hash_func = getattr(cls,hash_func_name)
                 txt = hash_func(txt)
             return txt 
-    
+
+        cls.custom_convert_to_hash = convert_to_hash
         return convert_to_hash
 
+    @classmethod
+    def custom_convert_to_hash(cls,txt):
+        return cls.convert_to_hash(txt)
 
     @classmethod 
     def create_list(cls, Key, size,add_to_half = 1):
         keys = array([Key])
+
         for _ in range(get_nearist_2_power_until_64(size)):
             keys = cls.create_list_helper(keys,add_to_half)
             
@@ -66,19 +72,21 @@ class Keys:
     @classmethod
     def split_and_hash(cls, key,add_to_half = 1):
         half_key = (len(key) // 2) + add_to_half
-        return [cls.convert_to_hash(key[half_key:]),cls.convert_to_hash(key[:half_key])]
+        return [cls.custom_convert_to_hash(key[half_key:]), cls.custom_convert_to_hash(key[:half_key])]
 
     # takes a list of hashes and return a twice as big list from spliting anf hashing each hash
     @classmethod
     def create_list_helper(cls, keys, add_to_half=1 ):
         new_keys = empty((len(keys) * 2),dtype='<U65')
         idx = 0 
+
         for key in keys:
             left,right = cls.split_and_hash(key, add_to_half)
             new_keys[idx] = left
             idx+=1 
             new_keys[idx] = right
             idx += 1 
+
         return new_keys
 
     # write the  generated list from create_list_helper in a file called keys.txt
@@ -88,15 +96,15 @@ class Keys:
         result = "".join(keys_list)
         return array([ord(_char) for _char in result]),keys_list
 
-    """ takes the size of the text that to be encrypted  and takes the key to genrate the file (keys.txt) using the class
+    """ takes the size of the text that to be encrypted  and takes the key to genrate keylist using the class
      method create_hash_list adn then take  the  series of hashes and pass it to the nums classmethod"""
     @classmethod
     def generate_keylist(cls, txt_size, key, add_to_half = 1,case="NONE"):
-        key,_ = cls.create_hash_list(key, txt_size,add_to_half)
+        hash_keys,_ = cls.create_hash_list(key, txt_size,add_to_half)
         num_list = []
 
-        for idx in range(1, (len(key) // 5) + 1):
-            temp_list = key[(idx * 5) - 5  :idx * 5]
+        for idx in range(1, (len(hash_keys) // 5) + 1):
+            temp_list = hash_keys[(idx * 5) - 5  :idx * 5]
             num_list.append(cls.generate_nums(temp_list,case)) 
 
         return num_list
@@ -110,7 +118,7 @@ class Keys:
         temp_list = [ int(num) for num in temp_list]
 
         if len(temp_list) < 4:
-            raise ValueError("Templist should be consist of at least 5 items")
+            raise ValueError("Templist should  consist of at least 5 items")
 
         try:
             num1 += int((((temp_list[2] ** temp_list[1]) +
