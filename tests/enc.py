4eca8a4fc87602a4c15a80b9636778fd1fcd007f
Ali-Al-Hadi-Al-Husseini <alihadi.alhusseini@gmail.com>
2023-01-31 13:24:16 +0200

    added get_characters_list to exports

--- tests/enc.py
+++ tests/enc.py
@@ -9,4 +9,4 @@ sys.path.append('c:\\Users\\lilo\\Documents\\GitHub\\AEH_Encryption\\Enc\\Encryp
 from Enc import *
 
 
-__all__ = ['Block_Tools','Shuffle','Block','get_characters_list','Dict_Tools','Keys','String_tools','AE']
+__all__ = ['Block_Tools','Shuffle','Block','get_characters_list','Dict_Tools','Keys','String_tools','AE','get_characters_list']
