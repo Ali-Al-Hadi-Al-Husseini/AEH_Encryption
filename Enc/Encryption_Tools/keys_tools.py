98b608a7eb35cc130242cf42ae3c82e1e0670eb4
Ali-Al-Hadi-Al-Husseini <alihadi.alhusseini@gmail.com>
2023-02-18 20:08:54 +0200

    chnaged variable names

--- Enc/Encryption_Tools/keys_tools.py
+++ Enc/Encryption_Tools/keys_tools.py
@@ -112,40 +112,40 @@ class Keys:
     def generate_nums(cls, numbers_list: List[int], Case: str ='NONE') -> int : 
 
         len_chars = len(get_characters_list())
-        num1 = 0
+        new_number = 0
         numbers_list = [ int(num) for num in numbers_list]
 
         if len(numbers_list) < 4:
             raise ValueError("Templist should  consist of at least 5 items")
 
         try:
-            num1 += int((((numbers_list[2] ** numbers_list[1]) ^
+            new_number += int((((numbers_list[2] ** numbers_list[1]) ^
                         numbers_list[0]) * numbers_list[4]) // numbers_list[3]) % len_chars
             # in cases where numbers_list[3] can be zero so thats why we used try/execpt here
             if Case != 'NONE':
-                if num1 == 0:
-                    num1 = int(
+                if new_number == 0:
+                    new_number = int(
                         (((numbers_list[1] ** numbers_list[3]) + numbers_list[2]) ^ numbers_list[4]) // numbers_list[0]) % len_chars
 
-                if num1 == 0:
-                    num1 = int((  (numbers_list[0] + numbers_list[1] +
+                if new_number == 0:
+                    new_number = int((  (numbers_list[0] + numbers_list[1] +
                             numbers_list[2] ) ** (numbers_list[3] ^ numbers_list[4])) // 3) % len_chars
 
 
         except ZeroDivisionError:
-            # provides two alternative number if num1 has an error
+            # provides two alternative number if new_number has an error
             try:
-                num1 = int(
+                new_number = int(
                     (((numbers_list[1] ** numbers_list[3]) ^ numbers_list[2]) * numbers_list[4]) // numbers_list[0]) % len_chars
 
             except ZeroDivisionError:
-                """num1 cann't have an error because it uses only addtion it can only be 0 if all the i
+                """new_number cann't have an error because it uses only addtion it can only be 0 if all the i
                 tems in numbers_list is zero which is rare to happen """
 
-                num1 = int((  (numbers_list[0] ^ numbers_list[1] ^
+                new_number = int((  (numbers_list[0] ^ numbers_list[1] ^
                         numbers_list[2] ) ** (numbers_list[3] * numbers_list[4])) // 3) % len_chars
 
-        return num1
+        return new_number
 
 def get_nearist_2_power_until_64(num: int) -> int:
     counter = 0
